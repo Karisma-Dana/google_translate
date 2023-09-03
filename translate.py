4717e4f11169a85cae682198e398453c105272f3
Karisma-Dana <132178937+Karisma-Dana@users.noreply.github.com>
2023-09-03 11:42:35 +0800

    Add files via upload

---
+++ translate.py
@@ -0,0 +1,174 @@
+from googletrans import *
+import os
+translator= Translator()
+
+
+def header():
+    
+    
+    os.system("cls")
+    print(f"{'WELCOME':^40}")
+    print(f"{'D_NA TRANSLATE':^40}")
+    print(f"{'-'*40:^40}\n") 
+LANGUAGES = {
+    'af': 'afrikaans',
+    'sq': 'albanian',
+    'am': 'amharic',
+    'ar': 'arabic',
+    'hy': 'armenian',
+    'az': 'azerbaijani',
+    'eu': 'basque',
+    'be': 'belarusian',
+    'bn': 'bengali',
+    'bs': 'bosnian',
+    'bg': 'bulgarian',
+    'ca': 'catalan',
+    'ceb': 'cebuano',
+    'ny': 'chichewa',
+    'zh-cn': 'chinese (simplified)',
+    'zh-tw': 'chinese (traditional)',
+    'co': 'corsican',
+    'hr': 'croatian',
+    'cs': 'czech',
+    'da': 'danish',
+    'nl': 'dutch',
+    'en': 'english',
+    'eo': 'esperanto',
+    'et': 'estonian',
+    'tl': 'filipino',
+    'fi': 'finnish',
+    'fr': 'french',
+    'fy': 'frisian',
+    'gl': 'galician',
+    'ka': 'georgian',
+    'de': 'german',
+    'el': 'greek',
+    'gu': 'gujarati',
+    'ht': 'haitian creole',
+    'ha': 'hausa',
+    'haw': 'hawaiian',
+    'iw': 'hebrew',
+    'he': 'hebrew',
+    'hi': 'hindi',
+    'hmn': 'hmong',
+    'hu': 'hungarian',
+    'is': 'icelandic',
+    'ig': 'igbo',
+    'id': 'indonesian',
+    'ga': 'irish',
+    'it': 'italian',
+    'ja': 'japanese',
+    'jw': 'javanese',
+    'kn': 'kannada',
+    'kk': 'kazakh',
+    'km': 'khmer',
+    'ko': 'korean',
+    'ku': 'kurdish (kurmanji)',
+    'ky': 'kyrgyz',
+    'lo': 'lao',
+    'la': 'latin',
+    'lv': 'latvian',
+    'lt': 'lithuanian',
+    'lb': 'luxembourgish',
+    'mk': 'macedonian',
+    'mg': 'malagasy',
+    'ms': 'malay',
+    'ml': 'malayalam',
+    'mt': 'maltese',
+    'mi': 'maori',
+    'mr': 'marathi',
+    'mn': 'mongolian',
+    'my': 'myanmar (burmese)',
+    'ne': 'nepali',
+    'no': 'norwegian',
+    'or': 'odia',
+    'ps': 'pashto',
+    'fa': 'persian',
+    'pl': 'polish',
+    'pt': 'portuguese',
+    'pa': 'punjabi',
+    'ro': 'romanian',
+    'ru': 'russian',
+    'sm': 'samoan',
+    'gd': 'scots gaelic',
+    'sr': 'serbian',
+    'st': 'sesotho',
+    'sn': 'shona',
+    'sd': 'sindhi',
+    'si': 'sinhala',
+    'sk': 'slovak',
+    'sl': 'slovenian',
+    'so': 'somali',
+    'es': 'spanish',
+    'su': 'sundanese',
+    'sw': 'swahili',
+    'sv': 'swedish',
+    'tg': 'tajik',
+    'ta': 'tamil',
+    'te': 'telugu',
+    'th': 'thai',
+    'tr': 'turkish',
+    'uk': 'ukrainian',
+    'ur': 'urdu',
+    'ug': 'uyghur',
+    'uz': 'uzbek',
+    'vi': 'vietnamese',
+    'cy': 'welsh',
+    'xh': 'xhosa',
+    'yi': 'yiddish',
+    'yo': 'yoruba',
+    'zu': 'zulu',}
+while True :     
+    header()
+    count = 0
+    print(f"{'BAHASA YANG TERSEDIA':^40}\n")
+    for key,value in LANGUAGES.items() : 
+        count +=1 
+        print(f"{count}. {value}")
+    print(f"\n{'-'*40:^40}")
+
+
+    source_language = input("dari bahasa      = ")
+    target_language = input("ke bahasa apa    = ")
+    user_language = input(f"masukan kata atau kalimat yang menggunakan bahasa {source_language} = ")
+    count = 0
+    for key,value in LANGUAGES.items(): 
+        count += 1 
+        if value == source_language: 
+            user_source = key
+        
+                
+    for key,value in LANGUAGES.items():
+        if value == target_language : 
+            target_user = key 
+    
+    translate_process = translator.translate(text=user_language,src=user_source,dest=target_user,)
+    print(f"\n{'-'*40:^40}\n") 
+    print("HASIL TRANSLATE : \n")
+    print(f"{translate_process.text}")
+    print(f"\n{'-'*40:^40}\n")
+    user_final = input ("apakah mau lanjut lagi (y/n) = ")
+    if user_final == "y":
+        continue 
+    elif user_final == "n":
+        break 
+print("THANK YOU BABY")
+
+
+  
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
